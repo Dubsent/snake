83247570bdac54d3325d7821a0373f512052818d
dubsent <dubsent@gmail.com>
2020-02-26 22:19:15 +0300

    Создание конструктора. Принцип инкапсуляции

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -15,9 +15,6 @@ namespace snake
             p1.Draw();
 
             Point p2 = new Point(4, 5, '+');
-            /*p2.x = 4;
-            p2.y = 1;
-            p2.sym = '#';*/
             p2.Draw();
 
             Console.ReadLine();
