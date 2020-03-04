0e129f27ef57aef738a7fe9a896a944c163a8f4f
dubsent <dubsent@gmail.com>
2020-03-04 22:46:18 +0300

    Collision handling

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,25 +6,18 @@ namespace snake
 {
     class Program
     {
-        const int MINXY = 0;
-        const char HVLINE = '*';
         const int WIDTH = 100;
         const int HEIGHT = 40;
+
         static void Main(string[] args)
         {
-            //Console.SetWindowSize(1, 1);
+            Console.SetWindowSize(1, 1);
             Console.SetWindowSize(WIDTH + 2, HEIGHT + 1);
             Console.SetBufferSize(WIDTH + 2, HEIGHT + 1);
-            
 
-            HorizontalLine topline = new HorizontalLine(MINXY, WIDTH, MINXY, HVLINE);
-            HorizontalLine bottomline = new HorizontalLine(MINXY, WIDTH, HEIGHT, HVLINE);
-            VerticalLine leftline = new VerticalLine(MINXY, HEIGHT, MINXY, HVLINE);
-            VerticalLine rightline = new VerticalLine(MINXY, HEIGHT, WIDTH, HVLINE);
-            topline.DrawLine();
-            bottomline.DrawLine();
-            leftline.DrawLine();
-            rightline.DrawLine();
+            Walls walls = new Walls(WIDTH, HEIGHT);
+            walls.Draw();
+            
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
@@ -37,6 +30,11 @@ namespace snake
 
             while(true)
             {
+                
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
---
+++ snake/snake/Walls.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            
+            HorizontalLine topline = new HorizontalLine(0, mapWidth, 0, '+');
+            HorizontalLine bottomline = new HorizontalLine(0, mapWidth, mapHeight, '+');
+            VerticalLine leftline = new VerticalLine(0, mapHeight, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, mapHeight, mapWidth, '+');
+            
+            wallList.Add(topline);
+            wallList.Add(bottomline);
+            wallList.Add(leftline);
+            wallList.Add(rightline);
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            } return false;
+        }
+
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.DrawLine();
+            }
+        }
+    }
+}
